58c66a001ca1ad38b2575a3fe7e0d908e67cda9f
David <davidgbanks91@gmail.com>
2018-03-20 12:57:54 -0700

    Refactor Employee component into css and helper functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,11 +20,7 @@ class App extends Component {
       const employees = this.state.employees;
       return (
         <div style={{display:'flex', justifyContent:'center'}}>
-          <div style={{
-            border:'1px solid black',
-            borderRadius:'10px',
-            width:'75%'
-          }}>
+          <div style={{width:'75%'}}>
             {
               employees.map(employee => (
                 <Employee key={employee.id} employee={employee} opacity={0.7}/>
--- frontend/src/employee.js
+++ frontend/src/employee.js
@@ -1,37 +1,60 @@
 import React from 'react';
 import {Employee as Subordinate} from './employee';
+import './employee.css';
 
 export class Employee extends React.Component {
   constructor() {
     super();
-    // this.state = { showSubordinates: true };
+    this.state = { showSubordinates: true };
+  }
+
+  renderToggleControl(employee) {
+    if (employee.direct_reports) {
+      if (this.state.showSubordinates) {
+        return (
+          <div onClick={() => this.setState({showSubordinates:false})}>
+            Hide Direct Reports
+          </div>
+        );
+      } else {
+        return (
+          <div onClick={() => this.setState({showSubordinates:true})}>
+            Show Direct Reports
+          </div>
+        );
+      }
+    }
+  }
+
+  renderSubordinates(employee) {
+    if (employee.direct_reports && this.state.showSubordinates) {
+      return (
+        employee.direct_reports.map(subordinate => (
+          <Subordinate
+            key={subordinate.id}
+            employee={subordinate}
+            opacity={this.props.opacity - 0.1}
+          />
+        ))
+      );
+    }
   }
 
   render() {
     const employee = this.props.employee;
-    const hasDirectReports = Boolean(employee.direct_reports);
     const opacity = this.props.opacity;
+    
     return (
-      <div style={{border:'1px solid black', borderRadius:'10px'}}>
-        <div style={{
-          border: '1px solid black',
-          padding: '0px 20px',
-          backgroundColor: `rgba(255,0,0,${opacity})`,
-          borderRadius: '10px'
-        }}>
+      <div style={{borderLeft:'1px solid black', borderRadius:'10px'}}>
+        <div
+          className='employee'
+          style={{backgroundColor:`rgba(255,0,0,${opacity}`}}
+        >
           <h1>{employee.name}, {employee.title}</h1>
+          {this.renderToggleControl(employee)}
         </div>
         <div style={{width:'95%', marginLeft:'5%'}}>
-          {
-            hasDirectReports ?
-            employee.direct_reports.map(subordinate => (
-              <Subordinate
-                key={subordinate.id}
-                employee={subordinate}
-                opacity={opacity - 0.1}
-              />
-            )) : null
-          }
+          {this.renderSubordinates(employee)}
         </div>
       </div>
     );
